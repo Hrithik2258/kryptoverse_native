54f1957449f089fbfaade198f74f0b3324adbc29
Hrihtik Pawar <86373306+Hrithik2258@users.noreply.github.com>
2022-07-29 19:03:53 +0530

    first commit

---
+++ constants/dummy.js
@@ -0,0 +1,270 @@
+import assets from "./assets";
+
+const NFTData = [
+  {
+    id: "NFT-01",
+    name: "Abstracto #312",
+    creator: "Hrithik",
+    price: 4.25,
+    description:
+      "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural. The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural. The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural.",
+    image: assets.nft01,
+    bids: [
+      {
+        id: "BID-11",
+        name: "Jessica Tan",
+        price: 4.25,
+        image: assets.person02,
+        date: "December 12, 2019 at 12:10 PM",
+      },
+      {
+        id: "BID-12",
+        name: "Jennifer Sia",
+        price: 4.5,
+        image: assets.person03,
+        date: "December 27, 2019 at 1:50 PM",
+      },
+      {
+        id: "BID-13",
+        name: "Rosie Wong",
+        price: 4.75,
+        image: assets.person04,
+        date: "December 31, 2019 at 3:50 PM",
+      },
+    ],
+  },
+  {
+    id: "NFT-02",
+    name: "Green Coins",
+    creator: "Usmaan Koyla",
+    price: 7.25,
+    description:
+      "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural. Nulla sed velit erat vitae leo sem inceptos diam fames arcu hendrerit, quis ultrices in eleifend posuere ipsum conubia porttitor felis.",
+    image: assets.nft02,
+    bids: [
+      {
+        id: "BID-21",
+        name: "Jessica Tan",
+        price: 7.05,
+        image: assets.person04,
+        date: "December 12, 2019 at 12:10 PM",
+      },
+    ],
+  },
+  {
+    id: "NFT-03",
+    name: "NFT coins race",
+    creator: " Daaku  Managal Singh 🔫",
+    price: 95.25,
+    description:
+      "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural. Lorem ipsum dolor sit amet consectetur adipiscing elit consequat accumsan sapien, lectus convallis malesuada odio curae habitasse dignissim nascetur. Nulla sed velit erat vitae leo sem inceptos diam fames arcu hendrerit, quis ultrices in eleifend posuere ipsum conubia porttitor felis. Lorem ipsum dolor sit amet consectetur adipiscing elit consequat accumsan sapien, lectus convallis malesuada odio curae habitasse dignissim nascetur. Nulla sed velit erat vitae leo sem inceptos diam fames arcu hendrerit, quis ultrices in eleifend posuere ipsum conubia porttitor felis.",
+    image: assets.nft03,
+    bids: [
+      {
+        id: "BID-31",
+        name: "Jessica Tan",
+        price: 95.25,
+        image: assets.person02,
+        date: "December 12, 2019 at 12:10 PM",
+      },
+      {
+        id: "BID-32",
+        name: "Jennifer Sia",
+        price: 95.5,
+        image: assets.person03,
+        date: "December 27, 2019 at 1:50 PM",
+      },
+    ],
+  },
+  {
+    id: "NFT-04",
+    name: "Nifty NFT",
+    creator: "Putri Intan",
+    price: 54.25,
+    description:
+      "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural.Lorem ipsum dolor sit amet consectetur adipiscing elit consequat accumsan sapien, lectus convallis malesuada odio curae habitasse dignissim nascetur.",
+    image: assets.nft04,
+    bids: [
+      {
+        id: "BID-41",
+        name: "Jessica Tan",
+        price: 56.25,
+        image: assets.person02,
+        date: "December 12, 2019 at 12:10 PM",
+      },
+      {
+        id: "BID-42",
+        name: "Jennifer Sia",
+        price: 54.25,
+        image: assets.person03,
+        date: "December 27, 2019 at 1:50 PM",
+      },
+      {
+        id: "BID-43",
+        name: "Rosie Wong",
+        price: 55.15,
+        image: assets.person04,
+        date: "December 31, 2019 at 3:50 PM",
+      },
+      {
+        id: "BID-44",
+        name: "Vincent Swift",
+        price: 54.15,
+        image: assets.person02,
+        date: "December 31, 2019 at 3:50 PM",
+      },
+    ],
+  },
+  {
+    id: "NFT-05",
+    name: "Colorful circles",
+    creator: "Mokambo",
+    price: 10.25,
+    description:
+      "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural.",
+    image: assets.nft05,
+    bids: [
+      {
+        id: "BID-51",
+        name: "Jessica Tan",
+        price: 10.25,
+        image: assets.person02,
+        date: "December 12, 2019 at 12:10 PM",
+      },
+    ],
+  },
+  {
+    id: "NFT-06",
+    name: "Black box model",
+    creator: "Leo Messi",
+    price: 20.25,
+    description:
+      "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural. Lorem ipsum dolor sit amet consectetur adipiscing elit consequat accumsan sapien, lectus convallis malesuada odio curae habitasse dignissim nascetur. Nulla sed velit erat vitae leo sem inceptos diam fames arcu hendrerit, quis ultrices in eleifend posuere ipsum conubia porttitor felis.",
+    image: assets.nft06,
+    bids: [
+      {
+        id: "BID-61",
+        name: "Jessica Tan",
+        price: 20.25,
+        image: assets.person02,
+        date: "December 12, 2019 at 12:10 PM",
+      },
+      {
+        id: "BID-62",
+        name: "Jennifer Sia",
+        price: 20.5,
+        image: assets.person03,
+        date: "December 27, 2019 at 1:50 PM",
+      },
+      {
+        id: "BID-63",
+        name: "Rosie Wong",
+        price: 20.75,
+        image: assets.person04,
+        date: "December 31, 2019 at 3:50 PM",
+      },
+      {
+        id: "BID-64",
+        name: "Siti Nurhaliza",
+        price: 21.25,
+        image: assets.person02,
+        date: "December 31, 2019 at 3:50 PM",
+      },
+      {
+        id: "BID-65",
+        name: "Kaitlyn Lee",
+        price: 7.25,
+        image: assets.person02,
+        date: "December 31, 2019 at 3:50 PM",
+      },
+    ],
+  },
+  {
+    id: "NFT-07",
+    name: "Abstracto soulful art",
+    creator: "Bhulla 🐂",
+    price: 18.25,
+    description:
+      "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural. Lorem ipsum dolor sit amet consectetur adipiscing elit consequat accumsan sapien, lectus convallis malesuada odio curae habitasse dignissim nascetur. Nulla sed velit erat vitae leo sem inceptos diam fames arcu hendrerit, quis ultrices in eleifend posuere ipsum conubia porttitor felis. Ullamcorper platea penatibus ornare egestas nulla ligula hendrerit nisl suscipit sociosqu maximus, tincidunt aptent habitant purus pharetra ultrices dapibus laoreet nisi lacinia. Porta malesuada netus vel sapien conubia porttitor aliquam ut pretium ante litora molestie senectus magna egestas sociosqu, eget aliquet fames pharetra felis posuere varius fringilla quisque in arcu montes eu ullamcorper.",
+    image: assets.nft07,
+    bids: [],
+  },
+  {
+    id: "NFT-08",
+    name:" Choota Motta Bheem",
+    creator: "ToonToon Mausi",
+    price: 18.25,
+    description:
+      "Bheem is an extremely brave, strong and intelligent young boy. He often manages to solve everyone's problems which endears him to the townspeople of Dholakpur.",
+    image: assets.nft08,
+    bids: [],
+  },
+  {
+    id: "NFT-09",
+    name:" Doremon",
+    creator: "future walla nobita ka pota shivashi",
+    price: 18.25,
+    description:
+     "Doraemon is a fictional character in the Japanese manga and anime series of the same name created by Fujiko Fujio, the pen name of writing team Hiroshi Fujimoto and Motoo Abiko. He is a male robotic earless cat that travels back in time from the 22nd century to aid a preteen boy named Nobita",
+    image: assets.nft09,
+    bids: [],
+  },
+
+  {
+    id: "NFT-10",
+    name:"Daku mangal Singh",
+    creator: "Sholle Walle Chacha",
+    price:"♾ ye Bikau Nhi Hai ",
+    description:
+     " It is the Frictional Character in the indian Moviw where he roow the Whole down ,until two wholesome Guys Come there ,Those wholesome Guys Namely Jay and veeru defet the  Daaku mangal Singh and make the relief for whole Village and at Last Thakur takes hi revenge with the Daaku Mangal Singh ",
+    image: assets.nft10,
+    bids: [],
+  },
+
+  {
+    id: "NFT-11",
+    name:"Shakal ",
+    creator: "MagarMach Ka Bachha ",
+    price:500,
+    description:
+     "  Ajjeb Janwar Hai Kitna Bhi Khaye ,Bhuka hi rehata Hai this Made this Charcter even more  Intersting than ever Befor his Favourate animal is Crocodile Because  Ajjeb Jaanwar hai Kitna bhi Khaye Bhukha Hi rehata Hai  ",
+    image: assets.nft11,
+    bids: [],
+  },
+
+  {
+    id: "NFT-12",
+    name:"Shakal ",
+    creator: "Nagmani Ka Loob  ",
+    price:78,
+    description:
+     " Abb Bhai Log Iss Character Ke bare Main Jada Mat Pouch na q  Ki bhai Agar kisi Ko Problem HAi To aao Kabhi Haveli pe  ",
+    image: assets.nft12,
+    bids: [],
+  },
+
+
+  {
+    id: "NFT-13",
+    name:"Bhulla 🐂 ",
+    // creator: "Nagmani Ka Loob  ",
+    price:9,
+    description:
+     " Bhulla Rakhta Hai Khulla   ",
+    image: assets.nft13,
+    bids: [],
+  },
+
+  {
+    id: "NFT-13",
+    name:"Bhulla 🐂 ",
+    // creator: "Nagmani Ka Loob  ",
+    price:5,
+    description:
+     " Bhulla Rakhta Hai Khulla   ",
+    image: assets.nft13,
+    bids: [],
+  },
+];
+
+export { NFTData };
---
+++ constants/index.js
@@ -0,0 +1,6 @@
+import assets from "./assets";
+import { COLORS, SHADOWS, SIZES, FONTS } from "./theme";
+import { NFTData } from "./dummy";
+import { CircleButton,RectButton } from "../Components/Button";
+
+export { assets, COLORS, SHADOWS, SIZES, FONTS, NFTData,CircleButton,RectButton };
